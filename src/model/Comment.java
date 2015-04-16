2dc71b2a4c21da489b08830405aee2b256b0af6c
nguidi <neri3004@gmail.com>
2015-04-15 21:22:45 -0300

    Haciendo uso de los packages de java

---
+++ src/model/Comment.java
@@ -0,0 +1,14 @@
+package model;
+public class Comment {
+
+	// Atributo: Contenido del Comentario.
+	private String content;
+
+	// Constructor del comentario
+	public Comment(String nuevoContenido) {
+		// Setea el nuevo comentario al Comment
+		this.content = nuevoContenido;
+
+	}
+
+}
